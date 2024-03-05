7249d88d1032297716351d360711ac3efe4d6b06
Alex Bihas <alex.bigas.2004@gmail.com>
2024-03-05 22:36:36 +0200

    refactored functions into arrow oneliners

--- week1/script.js
+++ week1/script.js
@@ -1,61 +1,33 @@
 // 1 - Remove String Spaces
-function noSpace(x) {
-  return x.replace(/\s/g, '');
-}
+const noSpace = x => x.replace(/\s/g, '');
 
 // 2 - Remove First and Last Character
-function removeChar(str) {
-  return str.slice(1, -1);
-}
+const removeChar = str => str.slice(1, -1);
 
 // 3 - MakeUpperCase
-function makeUpperCase(str) {
-  return str.toUpperCase();
-}
+const makeUpperCase = str => str.toUpperCase();
 
 // 4 - Find Maximum and Minimum Values of a List
-function min(list) {
-  return Math.min(...list);
-}
+const min = list => Math.min(...list);
 
-function max(list) {
-  return Math.max(...list);
-}
+const max = list => Math.max(...list);
 
 // 5 Count of positives / sum of negatives
-function countPositivesSumNegatives(input) {
-  if (!input?.length) return [];
-
-  const resArr = input.reduce(
-    (acc, cur) => {
-      if (cur > 0) {
-        ++acc[0];
-      } else if (cur < 0) {
-        acc[1] += cur;
-      }
-      return acc;
-    },
-    [0, 0]
-  );
-
-  return resArr;
-}
+const countPositivesSumNegatives = input =>
+  !input?.length
+    ? []
+    : input.reduce(
+        (acc, cur) =>
+          cur > 0 ? [++acc[0], acc[1]] : cur < 0 ? [acc[0], acc[1] + cur] : acc,
+        [0, 0]
+      );
 
 // 6 - Beginner - Reduce but Grow
-function grow(x) {
-  return x.reduce((acc, cur) => acc * cur, 1);
-}
+const grow = x => x.reduce((acc, cur) => acc * cur, 1);
 
 // 7 - Removing Elements
-function removeEveryOther(arr) {
-  return arr.filter((_, i) => i % 2 === 0);
-}
+const removeEveryOther = arr => arr.filter((_, i) => i % 2 === 0);
 
 // 8 - Calculate average
-function findAverage(array) {
-  if (!array.length) return 0;
-
-  const sum = array.reduce((acc, cur) => acc + cur);
-
-  return sum / array.length;
-}
+const findAverage = array =>
+  !array.length ? 0 : array.reduce((acc, cur) => acc + cur) / array.length;
